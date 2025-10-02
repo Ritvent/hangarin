f5b7d5e2dca8725295b9d846a3a0d4fcf1dfec32
Ritvent <ivanventurilloq@gmail.com>
2025-10-03 02:03:10 +0800

    integrate template

--- projectsite/hangarinorg/views.py
+++ projectsite/hangarinorg/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from hangarinorg.models import Task 
 
 # Create your views here.
+
+class HomePageView(ListView):
+    model = Task
+    template_name = 'base.html'
+    context_object_name = 'tasks'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add extra context if needed (categories, priorities, etc.)
+        return context
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'base.html'
+    context_object_name = 'tasks'
